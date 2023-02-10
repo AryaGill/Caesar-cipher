4c0c14a9bc78486edbcb755b6dfba2599303ff72
Arya Gill <aryagill43@icloud.com>
2023-02-09 17:12:34 -0800

    Add comments to improve code comprehension for others
    Added comments to explain each function. This helps others to understand the code without having to go through the code line by line.

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,6 +1,7 @@
 import argparse
 
 if __name__ == '__main__':
+    # Setting up argparse
     parse = argparse.ArgumentParser(
         description = "A CLI tool built with Python 3 designed to encrypt, decrypt, and brute force Caesar Cipher"
     )
@@ -16,6 +17,14 @@ if __name__ == '__main__':
 letters = "abcdefghijklmnopqrstuvwxyz"
 
 def encrypt_text(plaintext, n):
+    """
+    It takes a string and an integer as input, and returns a string that is the input string encrypted
+    by shifting each letter by the input integer
+    
+    :param plaintext: the text to be encrypted
+    :param n: the number of letters to shift by
+    :return: The encrypted text is being returned.
+    """
     encrypted = ""
     for i in range (len(plaintext)):
         char = plaintext[i]
@@ -34,6 +43,13 @@ def encrypt_text(plaintext, n):
     return encrypted
 
 def decrypt_text(encrypted, n):
+    """
+    It takes a string and a number, and shifts each letter in the string by the number
+    
+    :param encrypted: the encrypted text
+    :param n: the number of times the encryption will be repeated
+    :return: The decrypted text is being returned.
+    """
     decrypted = ""
     for i in range (len(encrypted)):
         char = encrypted[i]
@@ -52,6 +68,12 @@ def decrypt_text(encrypted, n):
     return(decrypted)
 
 def decrypt_text_brute(message):
+    """
+    For each key in the range of the length of the letters list, translate the message by shifting each
+    letter by the key value
+    
+    :param message: The message to be decrypted
+    """
     message = message.lower()
     for key in range(len(letters)):
         translated = ''
@@ -66,6 +88,10 @@ def decrypt_text_brute(message):
                 translated += char
         print('Key: %s - %s' % (key, translated))
 
+# This is a conditional statement that checks if the user has entered the -d flag, if so, it will
+# decrypt the text using the key provided by the user. If the user has entered the -b flag, it will
+# decrypt the text using brute force. If the user has entered the -e flag, it will encrypt the text
+# using the key provided by the user. The -t flag is always used to supply the encrypted or decrypted text.
 if (args.d): # decrypt_text
     print(decrypt_text(args.t, args.k))
 elif (args.b): # decrypt_text_brute
